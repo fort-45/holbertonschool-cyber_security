cac1f458e7fa88ecc84d9cf44c007b61457bd30f
Ayxan <ayxanps42007@gmail.com>
2025-12-26 07:46:43 -0500

    same

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-whois $1  | awk '/^Registrant/ || /^Admin/ || /^Tech/' > $1.csv
+#!/bin/bash
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/{s=$1} /:/{gsub(/^[ \t]+/,"",$2); if($1~/(Street)/) printf "%s %s,%s \n",s,$1,$2; else if($1~/(Phone Ext)/) printf "%s %s:, %s\n",s,$1,$2; else printf "%s %s,%s\n",s,$1,$2}' > "$1.csv"
