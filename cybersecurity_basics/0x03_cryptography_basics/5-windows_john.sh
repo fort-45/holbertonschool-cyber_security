2f7875a9baf0b3696d3da50cf5742736c40ebd9f
Ayxan <ayxanps42007@gmail.com>
2026-02-18 08:47:20 -0500

    same

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --format=nt --show "$1" | awk -F: '{print $2}' > 5-password.txt
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --format=nt --show "$1" | awk -F: 'NR==1{printf "%s", $2}' > 5-password.txt
