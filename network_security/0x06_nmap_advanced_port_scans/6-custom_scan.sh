212151dd73c524b523845d7f68f8f6fca90ee5dd
Ayxan <ayxanps42007@gmail.com>
2026-03-18 12:46:04 -0400

    same

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -scanflag URGACKPSHRSTSYNFIN -p "$2" "$1" -oN custom_scan.txt &> /dev/null
+sudo nmap --scanflag URGACKPSHRSTSYNFIN -p "$2" "$1" -oN custom_scan.txt &> /dev/null
